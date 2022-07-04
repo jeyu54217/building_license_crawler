d083168c93dd8a080464bc32773ea0cb09c4d024
Jerry Yu <jeyu54217.gmail.com>
2022-07-04 14:28:30 +0800

    Update, request with OCR & session

--- building_license_crawler/core_app/building_license_crawler.py
+++ building_license_crawler/core_app/building_license_crawler.py
@@ -1,8 +1,10 @@
 import time
 from pathlib import Path
+from urllib import response
 import requests
 import base64
 import json
+import shutil
 from selenium import webdriver
 
 start_time = time.time()  # timer
@@ -14,66 +16,75 @@ SOURCE_URL_2 = "https://building-management.publicwork.ntpc.gov.tw/bm_list.jsp"
 # CHROME_DRIVER_PATH = f"{Path(__file__).resolve().parent}\chromedriver.exe"
 OCR_PIC_PATH = 'C:\\Users\\Administrator\Desktop\\image.jpg'
 
-def get_cookie():
-    HEADERS = { 
-       'user-agent':  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36',
-       'Host': 'building-management.publicwork.ntpc.gov.tw',
-       'Referer': 'https://building-management.publicwork.ntpc.gov.tw/bm_query.jsp?rt=3',
-      }
-    rsp = requests.get(
-        SOURCE_URL_1,
-        headers = HEADERS,
-        )
-    cookies_1 = rsp.cookies['TS01983ad5']
-    cookies_2 = rsp.cookies['pcLevinSid'] 
+# def get_cookie():
+#     HEADERS = { 
+#        'user-agent':  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36',
+#        'Host': 'building-management.publicwork.ntpc.gov.tw',
+#        'Referer': 'https://building-management.publicwork.ntpc.gov.tw/bm_query.jsp?rt=3',
+#       }
+#     rsp = requests.get(
+#         SOURCE_URL_1,
+#         headers = HEADERS,
+#         )
+#     cookies_1 = rsp.cookies['TS01983ad5']
+#     cookies_2 = rsp.cookies['pcLevinSid'] 
     
-    return (cookies_1 , cookies_2)
+#     return (cookies_1 , cookies_2)
     
     
 def download_ocr_pic():
-    # browser = webdriver.Chrome(CHROME_DRIVER_PATH,)
-    # browser.get(SOURCE_URL)
-    # img_base64 = browser.execute_script(
-    #     """
-    #     var ele = arguments[0];
-    #     var cnv = document.createElement('canvas'); canvas
-    #     cnv.width = 252;
-    #     cnv.height = 72;
-    #     cnv.getContext('2d').drawImage(ele, 0, 0);
-    #     return cnv.toDataURL('image/jpeg').substring(22);
-    #     """, 
-    #     browser.find_element_by_xpath(
-    #         "//*[@id=\"realPic\"]"
-    #         )
-    #     )
-    
-    HEADERS = { 
-       'user-agent':  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36',
-       'Host': 'building-management.publicwork.ntpc.gov.tw',
-       'Referer': 'https://building-management.publicwork.ntpc.gov.tw/bm_query.jsp?rt=3',
-    #    'Cookie': f'pcLevinSid={cookies[0]}; TS01983ad5={cookies[1]}',
-      }
-    #  session test
-    s = requests.session()
-    s.headers = HEADERS
-    
-    
-    
+
+
     pic_url ='https://building-management.publicwork.ntpc.gov.tw/ImageServlet'
-    # Picture_request = requests.get(
-    #     pic_url,
-    #     headers = HEADERS,
-    #     )
     
-    Picture_request = s.get(
+    req = requests.session()
+    get_rsp = req.get(
         pic_url,
-        headers = HEADERS,
+        stream = True,
+        verify = False,
         )
     
-    if Picture_request.status_code == 200:
+    if get_rsp.status_code == 200:
         with open("C:\\Users\\Administrator\\Desktop\\image.jpg", 'wb') as f:
-            f.write(Picture_request.content)
-    s.close()
+            f.write(get_rsp.content)
+            f.close()
+            
+        with open(OCR_PIC_PATH, 'rb') as pic_file:
+            ocr_img64 = base64.b64encode(pic_file.read())
+
+        params = {
+            "username": "%s" % 'ctbc_china_biz1',
+            "password": "%s" % 'ctbcbank',
+            "captchaData": ocr_img64,
+            "captchaType": 1001,
+            }
+        
+        post_ocr_rsp = req.post("http://www.bingtop.com/ocr/upload/", data = params)
+        dictdata = json.loads(post_ocr_rsp.text)
+        ocr_code = dictdata['data']['recognition']
+        
+        
+        PAYLOAD = {
+            'rt': 'BM',
+            'PagePT': 0,
+            'A2': 3,
+            'F1':'1090102', # Date
+            'Z1': int(ocr_code),  # OCR code
+            }
+    
+        post_rsp = req.post(
+            SOURCE_URL_2,
+            data = PAYLOAD,
+            verify = False,
+            ).text  
+        
+        print(post_rsp)
+        
+        
+
+    
+
+    
     # with open(self.captcha_path, 'wb') as file:
     #         file.write(base64.b64decode(img_base64))
     #         file.close()
@@ -81,56 +92,80 @@ def download_ocr_pic():
             # return captcha_result
 
 # 冰拓_OCR : https://www.bingtop.com/
-def ocr_post():
-    with open(OCR_PIC_PATH, 'rb') as pic_file:
-        ocr_img64 = base64.b64encode(pic_file.read())
+def ocr_post(triger):
+    if triger == 'ok':
+        with open(OCR_PIC_PATH, 'rb') as pic_file:
+            ocr_img64 = base64.b64encode(pic_file.read())
 
-    params = {
-        "username": "%s" % 'ctbc_china_biz1',
-        "password": "%s" % 'ctbcbank',
-        "captchaData": ocr_img64,
-        "captchaType": 1001,
-        }
+        params = {
+            "username": "%s" % 'ctbc_china_biz1',
+            "password": "%s" % 'ctbcbank',
+            "captchaData": ocr_img64,
+            "captchaType": 1001,
+            }
     
-    response = requests.post("http://www.bingtop.com/ocr/upload/", data = params)
-    dictdata = json.loads(response.text)
-    ocr_code = dictdata['data']['recognition']
-    return ocr_code
+        response = requests.post("http://www.bingtop.com/ocr/upload/", data = params)
+        dictdata = json.loads(response.text)
+        ocr_code = dictdata['data']['recognition']
+        return ocr_code
 
     
-def main_crawler( ocr_code):
-    HEADERS = { 
-       'user-agent':  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36',
-       'Host': 'building-management.publicwork.ntpc.gov.tw',
-       'Referer': 'https://building-management.publicwork.ntpc.gov.tw/bm_query.jsp?rt=3',
-       'Cookie': f'pcLevinSid={cookies[0]}; TS01983ad5={cookies[1]}',
-       'Origin': 'https://building-management.publicwork.ntpc.gov.tw',
-      }
+def main_crawler(ocr_code):
+    # HEADERS = { 
+    #    'user-agent':  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36',
+    #    'Host': 'building-management.publicwork.ntpc.gov.tw',
+    #    'Referer': 'https://building-management.publicwork.ntpc.gov.tw/bm_query.jsp?rt=3',
+    #    'Cookie': f'pcLevinSid={cookies[0]}; TS01983ad5={cookies[1]}',
+    #    'Origin': 'https://building-management.publicwork.ntpc.gov.tw',
+    #   }
     
     PAYLOAD = {
         'rt': 'BM',
         'PagePT': 0,
         'A2': 3,
         'F1':'1090102', # Date
-        'Z1': ocr_code,  # OCR code
+        'Z1': int(ocr_code),  # OCR code
         }
     
-    output = requests.post(
+    response = requests.post(
         SOURCE_URL_2,
-        headers = HEADERS,
+        # headers = HEADERS,
         data = PAYLOAD,
-        ).text
+        verify = False,
+        ).text  
             
-    print(output)
+    print(response)
     
 if __name__ == '__main__':
-    pass
+    
     # keyword = '中國信託'
     # cookies = get_cookie()
     
-    # download_ocr_pic()
-    # ocr_code = ocr_post()
+    # triger = download_ocr_pic()
+    # ocr_code = ocr_post(triger)
     # main_crawler(ocr_code)
+    # download_ocr_pic()
+    
+    import pandas as pd
+    from datetime import date
+    start_date = date(2008, 8, 15) 
+    end_date = date(2008, 9, 15)    # perhaps date.now()
+
+    print(pd.date_range(start = start_date, end = end_date).strftime('%Y%m%d').tolist())
+    
+
+
+
+    # start_date = date(2008, 8, 15) 
+    # end_date = date(2008, 9, 15)    # perhaps date.now()
+
+    # delta = end_date - start_date   # returns timedelta
+
+    # print( [start_date + timedelta(days=i) for i in range(delta.days + 1)][3])
+    
+    
+        # day = start_date + timedelta(days=i)
+        # print(day)
 
 
 # for date diff
